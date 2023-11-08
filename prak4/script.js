82d9dfbc88c2ef7510e8b44889982a93e861ab56
121140208 Dea Lisriani Safitri Waruwu <118000973+Deawaruwu@users.noreply.github.com>
2023-11-08 21:50:13 +0700

    Add files via upload

---
+++ prak4/script.js
@@ -0,0 +1,21 @@
+//Nama : Dea Lisriani Safitri Waruwu
+//NIM  : 121140208
+//Kelas: RB
+document.getElementById("hitungButton").addEventListener("click", function() {
+    var inputBilangan = document.getElementById("bilangan").value;
+    var jumlahGanjil = 0;
+    var jumlahGenap = 0;
+
+    for (var i = 1; i <= inputBilangan; i++) {
+        if (i % 2 === 0) {
+            jumlahGenap++;
+        } else {
+            jumlahGanjil++;
+        }
+    }
+
+    var hasilPerhitungan = "Jumlah bilangan ganjil: " + jumlahGanjil + "<br>" +
+                          "Jumlah bilangan genap: " + jumlahGenap;
+
+    document.getElementById("hasil").innerHTML = hasilPerhitungan;
+});
